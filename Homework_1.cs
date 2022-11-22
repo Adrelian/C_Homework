c653c6b3907c9e9493680f712ad5b5d4736d1e7e
Anatolii <anatolii.polovnikov@gmail.com>
2022-11-22 20:40:51 +0500

    Решил задачу 6 и красиво оформил

--- Homework_1.cs
+++ Homework_1.cs
@@ -1,19 +1,19 @@
 ﻿//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее
 
 
-Console.Write("Input first number ");
-int FirNum = Convert.ToInt32(Console.ReadLine());
-Console.Write("Input second number ");
-int SecNum = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input first number ");
+// int FirNum = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input second number ");
+// int SecNum = Convert.ToInt32(Console.ReadLine());
 
-if (FirNum < SecNum)
-{
-    Console.WriteLine($"{FirNum}, {SecNum} -> max = {SecNum}");
-}
-else
-{
-    Console.WriteLine($"{FirNum}, {SecNum} -> max = {FirNum}");
-}
+// if (FirNum < SecNum)
+// {
+//     Console.WriteLine($"{FirNum}, {SecNum} -> max = {SecNum}");
+// }
+// else
+// {
+//     Console.WriteLine($"{FirNum}, {SecNum} -> max = {FirNum}");
+// }
 
 
 //Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
@@ -45,14 +45,14 @@ else
 
 //Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
 
-// Console.WriteLine("Input first number");
-// int Num = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input first number ");
+int Num = Convert.ToInt32(Console.ReadLine());
 
-//     if (Num % 2 == 0)
-//         Console.WriteLine($"{Num} -> чётное");
-//     else
-//         Console.WriteLine($"{Num} -> нечётное");
-//     Num += 1;
+    if (Num % 2 == 0)
+        Console.Write($"{Num} -> чётное");
+    else
+        Console.Write($"{Num} -> нечётное");
+    Num += 1;
 
 //Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
 
