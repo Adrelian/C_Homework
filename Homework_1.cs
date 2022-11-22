c869f014b9a4898cf8747908a1d72ec1449bfc5c
Anatolii <anatolii.polovnikov@gmail.com>
2022-11-22 19:33:05 +0500

    Домашняя работа, нужно оформить красивый вывод

---
+++ Homework_1.cs
@@ -0,0 +1,62 @@
+﻿//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее
+
+
+// Console.WriteLine("Input first number");
+// int FirNum = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input second number");
+// int SecNum = Convert.ToInt32(Console.ReadLine());
+
+// if (FirNum < SecNum)
+// {
+//     Console.WriteLine($"First Number  {FirNum} < {SecNum}");
+// }
+// else
+// {
+//     Console.WriteLine($"First Number  {FirNum} > {SecNum}");
+// }
+
+
+//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.WriteLine("Input first number");
+int FirNum = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input second number");
+int SecNum = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input third number");
+int ThirdNum = Convert.ToInt32(Console.ReadLine());
+
+if (FirNum < SecNum)
+{
+    if (SecNum < ThirdNum)
+    {
+        Console.WriteLine($"{ },{ }, {ThirdNum}");
+    }
+        
+    else
+        Console.WriteLine($"Max number is {SecNum}");
+}
+else
+Console.WriteLine($"Max number is {FirNum}");
+
+//Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+// Console.WriteLine("Input first number");
+// int Num = Convert.ToInt32(Console.ReadLine());
+
+//     if (Num % 2 == 0)
+//         Console.WriteLine($"{Num} -> чётное");
+//     else
+//         Console.WriteLine($"{Num} -> нечётное");
+//     Num += 1;
+
+//Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// Console.WriteLine("Input first number");
+// int Num = Convert.ToInt32(Console.ReadLine());
+// int count = 1;
+// while (count <= Num)
+// {
+//     if (count % 2 == 0)
+//         Console.WriteLine(count);
+//     count += 1;
+// }
