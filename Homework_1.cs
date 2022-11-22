cd30ba837e56fb27736da6c860972443179c5c78
Anatolii <anatolii.polovnikov@gmail.com>
2022-11-22 20:39:56 +0500

    Решил задачу 2 и красиво оформил

--- Homework_1.cs
+++ Homework_1.cs
@@ -1,46 +1,46 @@
 ﻿//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее
 
 
-// Console.WriteLine("Input first number");
-// int FirNum = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("Input second number");
-// int SecNum = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input first number ");
+int FirNum = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input second number ");
+int SecNum = Convert.ToInt32(Console.ReadLine());
 
-// if (FirNum < SecNum)
-// {
-//     Console.WriteLine($"First Number  {FirNum} < {SecNum}");
-// }
-// else
-// {
-//     Console.WriteLine($"First Number  {FirNum} > {SecNum}");
-// }
+if (FirNum < SecNum)
+{
+    Console.WriteLine($"{FirNum}, {SecNum} -> max = {SecNum}");
+}
+else
+{
+    Console.WriteLine($"{FirNum}, {SecNum} -> max = {FirNum}");
+}
 
 
 //Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 
 
-Console.Write("Input first number ");
-int FirNum = Convert.ToInt32(Console.ReadLine());
-Console.Write("Input second number ");
-int SecNum = Convert.ToInt32(Console.ReadLine());
-Console.Write("Input third number ");
-int ThirdNum = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input first number ");
+// int FirNum = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input second number ");
+// int SecNum = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input third number ");
+// int ThirdNum = Convert.ToInt32(Console.ReadLine());
 
-int max = 0;
-if (FirNum < SecNum)
-{
-    if (SecNum < ThirdNum)
-    {
-        max = ThirdNum; // макс третье число
-    }
+// int max = 0;
+// if (FirNum < SecNum)
+// {
+//     if (SecNum < ThirdNum)
+//     {
+//         max = ThirdNum; // макс третье число
+//     }
         
-    else
-        max = SecNum; // макс второе число
-}
-else
-max = FirNum; // макс первое число
+//     else
+//         max = SecNum; // макс второе число
+// }
+// else
+// max = FirNum; // макс первое число
 
-Console.Write($"{FirNum}, {SecNum}, {ThirdNum} - > {max}");
+// Console.Write($"{FirNum}, {SecNum}, {ThirdNum} - > {max}");
 
 
 //Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
